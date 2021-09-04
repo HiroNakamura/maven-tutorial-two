f4db4af1098cccb8f25af7b09db0d76799afafc6
Fer_Carraro <carraro.fernando@gmail.com>
2021-09-04 13:26:16 -0500

    Se han editado los submodulos del proyecto proyecto-maven

---
+++ proyectoJava/proyecto-maven/core/README.md
@@ -0,0 +1,14 @@
+Core
+
+# Sub módulo maven 
+
+**Listar dependencias compartidas, limpiar y construir**
+```bash
+$ mvn dependency:tree
+$ mvn clean package
+```
+
+**Ejecutar proyecto** 
+```bash
+$ mvn compile exec:java -Dexec.mainClass="org.curso.maven.App" [-Dexec.args="argument1"] ...
+```
--- proyectoJava/proyecto-maven/src/main/java/org/curso/maven/App.java
+++ proyectoJava/proyecto-maven/src/main/java/org/curso/maven/App.java
@@ -1,13 +1,11 @@
 package org.curso.maven;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ){
+        LOGGER.info("===== [MAVEN PROJECT: PARENT] =====");
     }
 }
